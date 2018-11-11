d17fcf58016dc35ea42e3c689e7c7b213a90f1f3
flaviowildner <flaviowildner@hotmail.com>
2018-11-11 02:26:35 -0200

    Corrigido diagrama de classe e adicionado novos diagramas de interações

--- src/Model/Administrador.java
+++
@@ -1,8 +0,0 @@
-package Model;
-
-public class Administrador extends Usuario{
-
-    public Administrador(String id, String senha, String nome){
-        super(id, senha, nome);
-    }
-}
--- src/Model/ZonaEleitoral.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-public class ZonaEleitoral {
-    private int regiao;
-
-    public ZonaEleitoral(int regiao){
-        this.regiao = regiao;
-    }
-}
